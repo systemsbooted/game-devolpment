9a41613fea3123bdd11c5589691b5e9ff14d613a
systemsbooted <106575797+systemsbooted@users.noreply.github.com>
2022-06-02 09:46:21 +1000

    Update randnum.js
    Fully working

--- randnum.js
+++ randnum.js
@@ -1,5 +1,4 @@
 var num = Math.floor(Math.random() * 10 + 1) ; 
-console.log("Welcome to systemsbooted's number guesser! Guess the number between 1 and 10.")
-const input = prompt();
+alert("Welcome to systemsbooted's number guesser! Guess the number between 1 and 10.")
 const input = prompt("Guess the number. Type a number between 1 and 10 below.");
-alert(`You guessed `);
+alert(`The you guessed is ${input}. The number was ${num}.`) 
